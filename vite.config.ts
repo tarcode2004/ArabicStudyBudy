dab8a616edbb6a30536831f465b8f903f163984b
tarcode2004 <66574422+tarcode2004@users.noreply.github.com>
2024-03-06 22:00:50 -0500

    Not sure

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
-import path from "path";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
+import path from "path"; // Import the 'path' module
 
 // https://vitejs.dev/config/
 export default defineConfig({
